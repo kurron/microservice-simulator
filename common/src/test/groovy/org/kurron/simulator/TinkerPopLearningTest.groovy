7b033cc454421e81b5bd4a5ed7082c3edaec1b9c
Ron Kurr <kurr@kurron.org>
2018-01-12 16:28:04 -0500

    Getting closer.

--- common/src/test/groovy/org/kurron/simulator/TinkerPopLearningTest.groovy
+++ common/src/test/groovy/org/kurron/simulator/TinkerPopLearningTest.groovy
@@ -1,12 +1,15 @@
 package org.kurron.simulator
 
 import static org.apache.tinkerpop.gremlin.process.traversal.P.eq
+import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.V
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.as as TinkerPopAs
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.both
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.cyclicPath
 import static org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__.unfold
 
+import org.apache.tinkerpop.gremlin.structure.Vertex
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerVertex
 import spock.lang.Specification
 
 class TinkerPopLearningTest extends Specification implements DataGenerator {
@@ -16,18 +19,33 @@ class TinkerPopLearningTest extends Specification implements DataGenerator {
 
         expect:
         def graph = TinkerGraph.open()
-        def vertices = (1..10).collect { graph.addVertex( it as String ) }
-        while(!vertices.empty) {
-            def from = vertices.pop()
-            def to = vertices.pop()
-            from.addEdge( 'linked', to )
+        def vertices = (1..6).collect {
+            def label = Integer.toHexString(it).toUpperCase()
+            graph.addVertex( 'name', label )
         }
+        def alpha = vertices.pop()
+        def bravo = vertices.pop()
+        def charlie = vertices.pop()
+        def delta = vertices.pop()
+        alpha.addEdge( 'linked', bravo )
+        bravo.addEdge( 'linked', charlie )
+        bravo.addEdge( 'linked', delta )
+
+        def echo = vertices.pop()
+        def foxtrot = vertices.pop()
+        echo.addEdge( 'linked', foxtrot )
+
+/*
+        while(!vertices.empty) {
+            def to = vertices.pop()
+            alpha.addEdge( 'linked', to )
+        }
+*/
 /*
         Vertex marko = graph.addVertex("name","marko","age",29)
         Vertex lop = graph.addVertex("name","lop","lang","java")
         marko.addEdge("created",lop,"weight",0.6d)
 */
-        //def result = graph.traversal().V().has("name","marko").out("created").values("name").toList()
 
         // http://tinkerpop.apache.org/docs/current/recipes/#connected-components
         def result = graph.traversal().V()
@@ -39,6 +57,8 @@ class TinkerPopLearningTest extends Specification implements DataGenerator {
                           .unfold().dedup().fold() )
                           .dedup()
                           .toList()
-        println( result )
+        result.each { List<Vertex> path ->
+            println path.collect { it.id() }.join( '->' )
+        }
     }
 }
